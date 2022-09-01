687ff49a5ba9bcc5f4e3e3fd553cb04b6ff1931d
Mike Weiblen <mew@mew.cx>
2022-09-01 14:22:29 -0600

    lots of tweaks

--- hpdl.py
+++ hpdl.py
@@ -4,8 +4,8 @@
 
 # hpdl.py
 
-__version__ = "0.0.0.0"
-__repo__ = "https://github.com/mew-cx/CircuitPython_hpdl.git"
+__version__ = "0.0.0.1"
+__repo__ = "https://github.com/mew-cx/CircuitPython_smart_display"
 
 import board
 import digitalio
@@ -16,13 +16,12 @@ import time
 #import gc
 #import sys
 #import micropython
-#from micropython import const
 
 #############################################################################
 
 class HPDL1414:
-    DATA_BITS = const(7)
-    ADDR_BITS = const(2)
+    DATA_BITS = 7
+    ADDR_BITS = 2
 
     def __init__(self, data_pins, addr_pins, wr_pin):
         if len(data_pins) != self.DATA_BITS:
@@ -30,8 +29,8 @@ class HPDL1414:
         if len(addr_pins) != self.ADDR_BITS:
             raise TypeError("addr_pins must have {} elements".format(self.ADDR_BITS))
 
-        self.data_pins = tuple([self.InitOutputPin(i) for i in data_pins])
-        self.addr_pins = tuple([self.InitOutputPin(i) for i in addr_pins])
+        self.data_pins = tuple(self.InitOutputPins(data_pins))
+        self.addr_pins = tuple(self.InitOutputPins(addr_pins))
         self.wr_pin = self.InitOutputPin(wr_pin)
 
     def __del__(self):
@@ -40,16 +39,18 @@ class HPDL1414:
         del self.wr_pin
 
     def __enter__(self):
+        raise NotImplementedException()
 
     def __exit__(self):
+        raise NotImplementedException()
 
     def deinit(self):
         (pin.deinit() for pin in self.data_pins)
         (pin.deinit() for pin in self.addr_pins)
         self.wr_pin.deinit()
 
-    def InitOutputPins(self, pins, pin_count):
-        raise NotImplementedException()
+    def InitOutputPins(self, pins):
+        return [self.InitOutputPin(i) for i in pins]
 
     def InitOutputPin(self, board_pin):
         pin = digitalio.DigitalInOut(board_pin)
@@ -59,7 +60,7 @@ class HPDL1414:
 
     def SetPins(self, iter, value):
         for pin in iter:
-            pin.value = value & 0x01
+            pin.value = bool(value & 0x01)
             value >>= 1
 
     def SetDataPins(self, value):
@@ -79,6 +80,7 @@ class HPDL1414:
         StrobeWrPin()
 
     def SetMessage(self, msg):
+        raise NotImplementedException()
 
 #############################################################################
 
@@ -98,7 +100,6 @@ ADDR_PINS = (
 )
 
 WR_PIN = board.D9
-
 
 a = HPDL1414(DATA_PINS, ADDR_PINS, WR_PIN)
 
