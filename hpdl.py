445898b6423123aaf4b99ce85a614b907b0ac973
Mike Weiblen <mew@mew.cx>
2022-09-13 03:27:29 -0600

    wip hpdl.py

--- hpdl.py
+++ hpdl.py
@@ -24,7 +24,7 @@ import microcontroller
 class PinBus:
     def __init__(self, board_pins, value=0):
         self._len = len(board_pins)
-        self._max_value = 1 << _self._len - 1
+        self._max_value = 1 << self._len - 1
         self._value = value
         plist = []
         for board_pin in board_pins:
@@ -36,7 +36,7 @@ class PinBus:
 
     def deinit(self):
         if not self._pins:
-            raise AttributeError(???)
+            raise ValueError("object was deinited.  create another.")
         [pin.deinit() for pin in self._pins]
         self._pins = None
         self._len = 0
@@ -46,28 +46,30 @@ class PinBus:
 
     max_value = property(lambda self: self._max_value)
 
-    def _set_value(self, value):
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
         """Write value to the pins"""
+        self._value = value
         for pin in self._pins:
             pin.value = bool(value & 0x01)
             value >>= 1
-        self._value = value
-
-    value = property(lambda self: self._value,
-                     self._set_value)
 
 #############################################################################
 
 class HPDL1414:
     ADDR_BITS = 2
-    DATA_BITS = 2 #7
+    DATA_BITS = 7
 
     def __init__(self, addr_pins, data_pins, wr_pin):
         """comment"""
         if len(addr_pins) != self.ADDR_BITS:
-            raise TypeError("addr_pins must have {} elements".format(self.ADDR_BITS))
+            raise ValueError("addr_pins must have {} elements".format(self.ADDR_BITS))
         if len(data_pins) != self.DATA_BITS:
-            raise TypeError("data_pins must have {} elements".format(self.DATA_BITS))
+            raise ValueError("data_pins must have {} elements".format(self.DATA_BITS))
 
         self._addr_pins = PinBus(addr_pins)
         self._data_pins = PinBus(data_pins)
@@ -95,26 +97,49 @@ class HPDL1414:
         self._wr_pin.value = True
 
     def SetMessage(self, msg):
-        raise NotImplementedException()
+        raise NotImplementedError()
 
 #############################################################################
 
-DATA_PINS = (
-    microcontroller.pin.PA23
-    microcontroller.pin.PA21
-    microcontroller.pin.PA20
-    microcontroller.pin.PA19
-    microcontroller.pin.PA17
-    microcontroller.pin.PA16
-    microcontroller.pin.PA18
-)
+# ItsyBitsy M0
+if False:
+    DATA_PINS = (
+        microcontroller.pin.PA23,
+        microcontroller.pin.PA21,
+        microcontroller.pin.PA20,
+        microcontroller.pin.PA19,
+        microcontroller.pin.PA17,
+        microcontroller.pin.PA16,
+        microcontroller.pin.PA18,
+    )
 
-ADDR_PINS = (
-    microcontroller.pin.PA15
-    microcontroller.pin.PA13
-)
+    ADDR_PINS = (
+        microcontroller.pin.PA15,
+        microcontroller.pin.PA13,
+    )
 
-WR_PIN = microcontroller.pin.PA12       # nWR red board.SDA
+    WR_PIN = microcontroller.pin.PA12       # nWR red board.SDA
+
+
+# RasPi Pico
+if True:
+    DATA_PINS = (
+        board.GP0,
+        board.GP1,
+        board.GP2,
+        board.GP3,
+        board.GP4,
+        board.GP5,
+        board.GP6,
+    )
+
+    ADDR_PINS = (
+        board.GP7,
+        board.GP8,
+    )
+
+    WR_PIN = board.GP9
+
 
 a = HPDL1414(ADDR_PINS, DATA_PINS, WR_PIN)
 
@@ -130,13 +155,13 @@ with HPDL1414(ADDR_PINS, DATA_PINS, WR_PIN) as b:
 print("DONE")
 
 #############################################################################
-nCE1 grn microcontroller.pin.PA04	board.A4
-nCE2 blu microcontroller.pin.PA06	board.A5
-
-nCLR vio microcontroller.pin.PA01	board.SCK
-
-nBL  blk microcontroller.pin.PA07	board.D2
-
-CUE  gry microcontroller.pin.PA00	board.MOSI
-nCU  wht microcontroller.pin.PB23	board.MISO
+#nCE1 grn microcontroller.pin.PA04	board.A4
+#nCE2 blu microcontroller.pin.PA06	board.A5
+#
+#nCLR vio microcontroller.pin.PA01	board.SCK
+#
+#nBL  blk microcontroller.pin.PA07	board.D2
+#
+#CUE  gry microcontroller.pin.PA00	board.MOSI
+#nCU  wht microcontroller.pin.PB23	board.MISO
 #############################################################################
