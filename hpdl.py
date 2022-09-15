ba1f83d6d32a5800a85898910a02dd5dcb10f233
Mike Weiblen <mew@mew.cx>
2022-09-15 13:25:49 -0600

    wip hpdl.py.  not tested.

--- hpdl.py
+++ hpdl.py
@@ -1,16 +1,15 @@
-# SPDX-FileCopyrightText: 2022 Michael Weiblen http://mew.cx/
+# SPDX-FileCopyrightText: 2022 Michael E. Weiblen http://mew.cx/
 #
 # SPDX-License-Identifier: MIT
 
 # hpdl.py
 
-__version__ = "0.0.1.0"
+__version__ = "0.0.1.1"
 __repo__ = "https://github.com/mew-cx/CircuitPython_smart_display"
 
 import board
 import digitalio
-import time
-import microcontroller
+from time import sleep
 
 #import mew_pinbus
 
@@ -22,21 +21,23 @@ import microcontroller
 #############################################################################
 
 class OutputPin:
+    """TODO"""
     def __init__(self, board_pin, value=0):
         self._pin = digitalio.DigitalInOut(board_pin)
-        self._pin.switch_to_output(bool(value & 0x01))
+        self._pin.switch_to_output(bool(value))
 
     def deinit(self):
-        assert self._pin, "object already deinited."
+        assert self._pin, "object already deinited."    # TODO type(self)
         self._pin.deinit()
         self._pin = None
 
     value = property(lambda self: 1 if self._pin.value else 0,
-            _value_setter, None,
+            lambda self, value: self._pin.value = bool(value),
+            None,
             "TODO value docstring")
 
     def strobe(self, duration=0):
-        assert self._pin, "object is deinited; create another."
+        assert self._pin, "object is deinited; create another." # TODO type(self)
         self._pin.value = not self._pin.value
         if duration:
             time.sleep(duration)
@@ -45,29 +46,30 @@ class OutputPin:
 #############################################################################
 
 class PinBus:
+    """TODO"""
     def __init__(self, board_pins, value=0):
         self._len = len(board_pins)
         self._max_value = 1 << self._len - 1
         self._value = value
         plist = []
         for bpin in board_pins:
-            plist.append(OutputPin(bpin, value))
+            plist.append(OutputPin(bpin, (value & 0x01)))
             value >>= 1
         self._pins = tuple(plist)
 
     def deinit(self):
-        assert self._pins, "object already deinited."
+        assert self._pins, "object already deinited."   # TODO type(self)
         [pin.deinit() for pin in self._pins]
         self._pins = None
         self._len = 0
         self._max_value = 0
 
     def _value_setter(self, value):
-        assert self._pins, "object is deinited; create another."
-        assert value>=0 and value<=self._max_value, "value is out of range."
+        assert self._pins, "object is deinited; create another."        # TODO type(self)
+        assert 0 <= value <= self._max_value, "value is out of range."
         self._value = value
         for pin in self._pins:
-            pin.value = value
+            pin.value = value & 0x01
             value >>= 1
 
     value = property(lambda self: self._value, _value_setter, None,
@@ -82,11 +84,13 @@ class PinBus:
 #############################################################################
 
 class HPDL1414:
+    """TODO"""
     ADDR_BITS = 2
     DATA_BITS = 7
+    NUM_CHARS = 4
 
     def __init__(self, addr_pins, data_pins, wr_pin):
-        """comment"""
+        """TODO"""
         if len(addr_pins) != self.ADDR_BITS:
             raise ValueError("addr_pins must have {} elements".format(self.ADDR_BITS))
         if len(data_pins) != self.DATA_BITS:
@@ -94,17 +98,15 @@ class HPDL1414:
 
         self._addr_pins = PinBus(addr_pins)
         self._data_pins = PinBus(data_pins)
-
-        self._wr_pin = digitalio.DigitalInOut(wr_pin)
-        self._wr_pin.switch_to_output()
+        self._wr_pin = OutputPin(wr_pin, 1)
 
     def __enter__(self):
         return self
 
-    def __exit__(self, a1, a2, a3):
+    def __exit__(self, exc_type, exc_val, exc_tb):
         self.deinit()
-        supress_exception = False       # supress exception from 'with'?
-        return supress_exception
+        # do not supress an exception occurring inside the context...
+        return False
 
     def deinit(self):
         print("deinit")
@@ -112,17 +114,31 @@ class HPDL1414:
         self._data_pins.deinit()
         self._wr_pin.deinit()
 
-    def SetChar(self, addr, data):
-        print("SetChar", addr, data)
+    def put(self, addr, data):
+        print("put", addr, data)
         self._addr_pins.value = addr
-        self._wr_pin.value = False
         self._data_pins.value = data
-        self._wr_pin.value = True
+        self._wr_pin.strobe()
 
-    def SetMessage(self, msg):
-        raise NotImplementedError()
+    def fill(self, value):
+        print("fill", value)
+        for i in range(NUM_CHARS):
+            put(i, value)
+
+    def print(self, msg):
+        print("print", msg)
+        assert len(msg) <= 4, "msg is too long."
+        for i,c in enumerate(msg):
+            put(NUM_CHARS - i - 1, ord(c))
+        #raise NotImplementedError()
 
 #############################################################################
+#class HPDL2416:
+#    """TODO"""
+# TODO list of CE values in order
+#############################################################################
+
+import microcontroller
 
 # ItsyBitsy M0
 if False:
@@ -166,7 +182,6 @@ if True:
 
 def main():
     a = HPDL1414(ADDR_PINS, DATA_PINS, WR_PIN)
-
     print(a)
     a.SetChar(1, 0)
     a.deinit()
