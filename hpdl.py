29475062b59f91b98c87f24dbf6c0fea67d90970
Mike Weiblen <mew@mew.cx>
2022-09-16 10:50:11 -0600

    add class HPDL2416: not working

--- hpdl.py
+++ hpdl.py
@@ -4,7 +4,7 @@
 
 # hpdl.py
 
-__version__ = "0.0.1.1"
+__version__ = "0.0.1.2"
 __repo__ = "https://github.com/mew-cx/CircuitPython_smart_display"
 
 import board
@@ -32,8 +32,7 @@ class OutputPin:
         self._pin = None
 
     value = property(lambda self: 1 if self._pin.value else 0,
-            lambda self, value: self._pin.value = bool(value),
-            None,
+            lambda self, value: bool(value), None,
             "TODO value docstring")
 
     def strobe(self, duration=0):
@@ -49,11 +48,11 @@ class PinBus:
     """TODO"""
     def __init__(self, board_pins, value=0):
         self._len = len(board_pins)
-        self._max_value = 1 << self._len - 1
+        self._max_value = (1 << self._len) - 1
         self._value = value
         plist = []
         for bpin in board_pins:
-            plist.append(OutputPin(bpin, (value & 0x01)))
+            plist.append(OutputPin(bpin, value & 0x01))
             value >>= 1
         self._pins = tuple(plist)
 
@@ -98,70 +97,130 @@ class HPDL1414:
 
         self._addr_pins = PinBus(addr_pins)
         self._data_pins = PinBus(data_pins)
-        self._wr_pin = OutputPin(wr_pin, 1)
+        self._wr_pin    = OutputPin(wr_pin, 1)
 
     def __enter__(self):
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.deinit()
-        # do not supress an exception occurring inside the context...
+        # False = dont supress an exception occurring inside the context
         return False
 
     def deinit(self):
-        print("deinit")
+        print(type(self), "deinit()")
         self._addr_pins.deinit()
         self._data_pins.deinit()
         self._wr_pin.deinit()
 
     def put(self, addr, data):
-        print("put", addr, data)
+        "Put a single character at specified position"
+        print(type(self), "put(", addr, data, ")")
+        assert 0 <= addr < NUM_CHARS, "addr is out of range."
+        assert isinstance(int, data), "data must be integer ascii code."
         self._addr_pins.value = addr
         self._data_pins.value = data
         self._wr_pin.strobe()
 
-    def fill(self, value):
-        print("fill", value)
+    def fill(self, data):
+        print(type(self), "fill(", data, ")")
         for i in range(NUM_CHARS):
-            put(i, value)
+            put(i, data)
 
     def print(self, msg):
-        print("print", msg)
-        assert len(msg) <= 4, "msg is too long."
+        print(type(self), "print(", msg, ")")
+        assert len(msg) <= NUM_CHARS, "msg is too long."
         for i,c in enumerate(msg):
             put(NUM_CHARS - i - 1, ord(c))
         #raise NotImplementedError()
 
 #############################################################################
-#class HPDL2416:
-#    """TODO"""
-# TODO list of CE values in order
+
+class HPDL2416(HPDL1414):
+    """TODO"""
+    CE_BITS = 2
+
+    def __init__(self, addr_pins, data_pins, wr_pin, ce_pins, nclr_pin, nbl_pin, cue_pin, ncu_pin):
+        """TODO"""
+        if len(ce_pins) != self.CE_BITS:
+            raise ValueError("ce_pins must have {} elements".format(self.CE_BITS))
+
+        super().__init__(addr_pins, data_pins, wr_pin)
+        self._ce_pins  = PinBus(ce_pins, 3)
+        self._nclr_pin = OutputPin(nclr_pin, 1)
+        self._nbl_pin  = OutputPin(nbl_pin, 1)
+        self._cue_pin  = OutputPin(cue_pin, 0)
+        self._ncu_pin  = OutputPin(ncu_pin, 1)
+
+    def deinit(self):
+        print(type(self), "deinit()")
+        self._ce_pins.deinit()
+        self._nclr_pin.deinit()
+        self._nbl_pin.deinit()
+        self._cue_pin.deinit()
+        self._ncu_pin.deinit()
+        super().deinit()
+
+    def chip_enable(self, value):
+        print(type(self), "chip_enable(", value, ")")
+        self._ce_pins.value = value
+
+    def clear(self):
+        print(type(self), "clear()")
+        self._nclr_pin.strobe(0.005)
+
+    def blank(self, value):
+        print(type(self), "blank(", value, ")")
+        self._nbl_pin.value(not bool(value))
+
+    def cursor_enable(self, value):
+        print(type(self), "cursor_enable(", value, ")")
+        self._cue_pin.value(bool(value))
+
+    def cursor_mode(self, value):
+        print(type(self), "cursor_mode(", value, ")")
+        self._ncu_pin.value(not bool(value))
+
 #############################################################################
 
 import microcontroller
 
 # ItsyBitsy M0
 if False:
-    DATA_PINS = (
-        microcontroller.pin.PA23,
-        microcontroller.pin.PA21,
-        microcontroller.pin.PA20,
-        microcontroller.pin.PA19,
-        microcontroller.pin.PA17,
-        microcontroller.pin.PA16,
-        microcontroller.pin.PA18,
-    )
-
     ADDR_PINS = (
-        microcontroller.pin.PA15,
-        microcontroller.pin.PA13,
+        microcontroller.pin.PA15,               # A0
+        microcontroller.pin.PA13,               # A1
     )
 
-    WR_PIN = microcontroller.pin.PA12       # nWR red board.SDA
+    DATA_PINS = (
+        microcontroller.pin.PA23,               # D0
+        microcontroller.pin.PA21,               # D1
+        microcontroller.pin.PA20,               # D2
+        microcontroller.pin.PA19,               # D3
+        microcontroller.pin.PA17,               # D4
+        microcontroller.pin.PA16,               # D5
+        microcontroller.pin.PA18,               # D6
+    )
 
+    WR_PIN = microcontroller.pin.PA12           # nWR red board.SDA
+
+    CE_PINS = (
+        microcontroller.pin.PA04,               # nCE1 grn board.A4
+        microcontroller.pin.PA06,               # nCE2 blu board.A5
+    )
+
+    nCLR_PIN = microcontroller.pin.PA01         # nCLR vio board.SCK
+    nBL_PIN  = microcontroller.pin.PA07         # nBL  blk board.D2
+    CUE_PIN  = microcontroller.pin.PA00         # CUE  gry board.MOSI
+    nCU_PIN  = microcontroller.pin.PB23         # nCU  wht board.MISO
 
 # RasPi Pico
 if True:
+    ADDR_PINS = (
+        board.GP7,
+        board.GP8,
+    )
+
     DATA_PINS = (
         board.GP0,
         board.GP1,
@@ -172,34 +231,24 @@ if True:
         board.GP6,
     )
 
-    ADDR_PINS = (
-        board.GP7,
-        board.GP8,
-    )
-
     WR_PIN = board.GP9
 
+#############################################################################
 
 def main():
     a = HPDL1414(ADDR_PINS, DATA_PINS, WR_PIN)
     print(a)
-    a.SetChar(1, 0)
+    a.put(1, 0)
     a.deinit()
-    print(__version__)
+
     with HPDL1414(ADDR_PINS, DATA_PINS, WR_PIN) as b:
-        b.SetChar(0, 2)
-    print("DONE")
+        b.put(0, 2)
+
+    x = HPDL2416(ADDR_PINS, DATA_PINS, WR_PIN,
+        CE_PINS, nCLR_PIN, nBL_PIN, CUE_PIN, nCU_PIN)
+
+    print(__version__)
 
 main()
 
-#############################################################################
-#nCE1 grn microcontroller.pin.PA04	board.A4
-#nCE2 blu microcontroller.pin.PA06	board.A5
-#
-#nCLR vio microcontroller.pin.PA01	board.SCK
-#
-#nBL  blk microcontroller.pin.PA07	board.D2
-#
-#CUE  gry microcontroller.pin.PA00	board.MOSI
-#nCU  wht microcontroller.pin.PB23	board.MISO
-#############################################################################
+# eof
